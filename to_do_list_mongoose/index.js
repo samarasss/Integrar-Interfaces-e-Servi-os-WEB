80c31ea763d0553f3b5f672524c841511e77201e
Samara Sampaio <samara.sampaio@lsd.ufcg.edu.br>
2025-02-11 15:56:48 -0300

    Projeto to-do-list conectado ao bd

---
+++ to_do_list_mongoose/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const taskRoutes = require("./route/taskRoute.js");
+const db = require("./db/db.js");
+
+const app = express();
+app.use(bodyParser.json()); // Para ler o corpo das requisições como JSON
+app.use("/api", taskRoutes);
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Aplicação rodando na porta ${port}`);
+});
