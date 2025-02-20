7022eec71b67a7439584523033cbb9460654546d
samarasss <samara.sampaio@lsd.ufcg.edu.br>
2025-02-20 18:35:53 -0300

    Update profile.js

--- to_do_list_mongoose - relacionamentos/model/profile.js
+++ to_do_list_mongoose - relacionamentos/model/profile.js
@@ -4,7 +4,7 @@ let profileSchema = new mongoose.Schema({
   occupation: String,
   phone: String,
   address: String,
-  person: { type: mongoose.Schema.Types.ObjectId, ref: "Person", unique: true }, 
+  person: { type: mongoose.Schema.Types.ObjectId, ref: "Person" }, 
 });
 
 module.exports = mongoose.model("Profile", profileSchema);
